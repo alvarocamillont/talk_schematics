bcd28334e3e8612039e052c8e9a46bc57e119196
Alvaro <alvaro.camilloneto@gmail.com>
2019-07-15 00:31:17 -0300

    Exemplo do olá mundo :)

--- meuSchematic/src/meu-schematic/index.ts
+++ meuSchematic/src/meu-schematic/index.ts
@@ -1,10 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function meuSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const { nome } = _options;
+
+    tree.create('hello.js', `console.log('Olá ${nome}');`);
+
     return tree;
   };
 }
