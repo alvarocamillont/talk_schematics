47def461afcf06acfba77f24abfc6d04e8ae7f11
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-07-20 11:03:43 -0300

    Correções de ultima hora

--- meuSchematic/src/meu-schematic/index.ts
+++ meuSchematic/src/meu-schematic/index.ts
@@ -1,11 +1,10 @@
 import { strings } from '@angular-devkit/core';
 import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
-import { Parametros } from './parametros.d';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function meuSchematic(_options: Parametros): Rule {
+export function meuSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const sourceTemplates = url('./files');
 
